3d9545ccf74013619444bdd7100d77776740c3ec
renweizhang23 <156152585+shikong1101@users.noreply.github.com>
2025-03-11 23:22:36 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace LengthCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.clear = new System.Windows.Forms.Button();
+            this.cm = new System.Windows.Forms.Label();
+            this.m = new System.Windows.Forms.Label();
+            this.km = new System.Windows.Forms.Label();
+            this.inch = new System.Windows.Forms.Label();
+            this.ft = new System.Windows.Forms.Label();
+            this.yard = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(56, 44);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(96, 29);
+            this.clear.TabIndex = 0;
+            this.clear.Text = "清除";
+            this.clear.UseVisualStyleBackColor = true;
+            // 
+            // cm
+            // 
+            this.cm.AutoSize = true;
+            this.cm.Location = new System.Drawing.Point(55, 99);
+            this.cm.Name = "cm";
+            this.cm.Size = new System.Drawing.Size(37, 15);
+            this.cm.TabIndex = 1;
+            this.cm.Text = "公分";
+            // 
+            // m
+            // 
+            this.m.AutoSize = true;
+            this.m.Location = new System.Drawing.Point(144, 99);
+            this.m.Name = "m";
+            this.m.Size = new System.Drawing.Size(37, 15);
+            this.m.TabIndex = 2;
+            this.m.Text = "公尺";
+            // 
+            // km
+            // 
+            this.km.AutoSize = true;
+            this.km.Location = new System.Drawing.Point(241, 99);
+            this.km.Name = "km";
+            this.km.Size = new System.Drawing.Size(37, 15);
+            this.km.TabIndex = 3;
+            this.km.Text = "公里";
+            // 
+            // inch
+            // 
+            this.inch.AutoSize = true;
+            this.inch.Location = new System.Drawing.Point(55, 170);
+            this.inch.Name = "inch";
+            this.inch.Size = new System.Drawing.Size(37, 15);
+            this.inch.TabIndex = 4;
+            this.inch.Text = "英吋";
+            // 
+            // ft
+            // 
+            this.ft.AutoSize = true;
+            this.ft.Location = new System.Drawing.Point(144, 170);
+            this.ft.Name = "ft";
+            this.ft.Size = new System.Drawing.Size(37, 15);
+            this.ft.TabIndex = 5;
+            this.ft.Text = "英尺";
+            // 
+            // yard
+            // 
+            this.yard.AutoSize = true;
+            this.yard.Location = new System.Drawing.Point(241, 170);
+            this.yard.Name = "yard";
+            this.yard.Size = new System.Drawing.Size(37, 15);
+            this.yard.TabIndex = 6;
+            this.yard.Text = "英碼";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 117);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(76, 25);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(76, 25);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(244, 117);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(76, 25);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(56, 188);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(76, 25);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(147, 188);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(76, 25);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(244, 188);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(76, 25);
+            this.textBox6.TabIndex = 12;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "說明文字";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(56, 265);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(164, 25);
+            this.textBox7.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.yard);
+            this.Controls.Add(this.ft);
+            this.Controls.Add(this.inch);
+            this.Controls.Add(this.km);
+            this.Controls.Add(this.m);
+            this.Controls.Add(this.cm);
+            this.Controls.Add(this.clear);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Label cm;
+        private System.Windows.Forms.Label m;
+        private System.Windows.Forms.Label km;
+        private System.Windows.Forms.Label inch;
+        private System.Windows.Forms.Label ft;
+        private System.Windows.Forms.Label yard;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
